952efc699a493032656b3463c474bfe9cd19e018
Your Name <you@example.com>
2023-04-19 15:08:19 -0700

    First two functions completed

---
+++ lib/high_score.ex
@@ -0,0 +1,27 @@
+defmodule HighScore do
+  @initial_score 0
+
+  def new() do
+    %{}
+  end
+
+  def add_player(scores, name, score \\ initial_score) do
+    Map.put(scores, name, score)
+  end
+
+  def remove_player(scores, name) do
+    # Please implement the remove_player/2 function
+  end
+
+  def reset_score(scores, name) do
+    # Please implement the reset_score/2 function
+  end
+
+  def update_score(scores, name, score) do
+    # Please implement the update_score/3 function
+  end
+
+  def get_players(scores) do
+    # Please implement the get_players/1 function
+  end
+end
